c720d6c366e5381503204bd56f6e7b269d49ea22
Erik Huang <ekhu94@gmail.com>
2021-04-19 11:23:52 -0700

    adds partials for header and footer, create randNum view

--- index.js
+++ index.js
@@ -2,13 +2,18 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
+
 app.listen(3000, () => {
   console.log("Listening on Port 3000");
 });
 
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "/views"));
-
 app.get("/", (req, res) => {
   res.render("index");
+});
+
+app.get("/random", (req, res) => {
+  const rand = Math.floor(Math.random() * 100) + 1;
+  res.render("random", { rand });
 });
